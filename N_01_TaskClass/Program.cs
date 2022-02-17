f64a4658414c5e43ad1a73995946cb34e294c635
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-17 14:49:39 +1000

    Добавил примеры: синхронный запуск и основные свойства

--- N_01_TaskClass/Program.cs
+++ N_01_TaskClass/Program.cs
@@ -10,7 +10,8 @@ public class Program
     {
         // SimpleExample();
         // WaitTasks();
-        WaitIsBlockMainThread();
+        // WaitIsBlockMainThread();
+        RunTaskSynchronously();
     }
 
     /// <summary>
@@ -70,5 +71,46 @@ public class Program
         task1.Wait(); // вызывающий поток так же будет заблокирован на 3 сек.
 
         Console.WriteLine("Main End");
+    }
+
+    /// <summary>
+    /// Синхронный запуск задачи
+    /// </summary>
+    private static void RunTaskSynchronously()
+    {
+        Console.WriteLine("Main start");
+
+        Task task1 = new Task(() =>
+        {
+            Console.WriteLine("Task start");
+            Thread.Sleep(3000);
+            Console.WriteLine("Task end");
+        });
+        
+        // Запускаем задачу синхронно
+        task1.RunSynchronously();
+
+        // Тест будет выведен только после синхронного выполнения задания
+        Console.WriteLine("Main end");
+    }
+
+    /// <summary>
+    /// Некоторые из основных свойств задания
+    /// </summary>
+    private static void TaskProperties()
+    {
+        Task task1 = new Task(() =>
+        {
+            Console.WriteLine($"Task{Task.CurrentId} start");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Task{Task.CurrentId} end");
+        });
+        task1.Start(); // Запускаем задачу
+
+        Console.WriteLine($"task1 Id: {task1.Id}");
+        Console.WriteLine($"task1 is Completed: {task1.IsCompleted}");
+        Console.WriteLine($"task1 Status: {task1.Status}");
+
+        task1.Wait();
     }
 }
