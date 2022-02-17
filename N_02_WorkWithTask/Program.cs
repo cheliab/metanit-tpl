81f5ab9cdf4bdee855015f531746800fd2367989
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-17 16:54:20 +1000

    Добавил пару примеров с массивами заданий

--- N_02_WorkWithTask/Program.cs
+++ N_02_WorkWithTask/Program.cs
@@ -1,11 +1,18 @@
 ﻿namespace N_02_WorkWithTask;
 
+// https://metanit.com/sharp/tutorial/12.2.php
+// 
+// Работа с классом Task
+
 class Program
 {
     public static void Main()
     {
         // InnerTasks_NotAttached();
-        InnerTasks_Attached();
+        // InnerTasks_Attached();
+        
+        // ArrayOfTask();
+        // ArrayOfTask_FactoryExample();
     }
 
     /// <summary>
@@ -58,4 +65,38 @@ class Program
 
         Console.WriteLine("End of Main");
     }
+
+    /// <summary>
+    /// Массив задач
+    /// </summary>
+    private static void ArrayOfTask()
+    {
+        Task[] tasks = new Task[3]
+        {
+            new Task(() => Console.WriteLine("First Task")),
+            new Task(() => Console.WriteLine("Second Task")),
+            new Task(() => Console.WriteLine("Third Task"))
+        };
+        
+        foreach (var t in tasks)
+            t.Start();
+        
+        // пока без ожидания
+    }
+
+    /// <summary>
+    /// Массив задач - Создание через фабрику
+    /// </summary>
+    private static void ArrayOfTask_FactoryExample()
+    {
+        Task[] tasks = new Task[3];
+
+        int j = 1;
+        for (int i = 0; i < tasks.Length; i++)
+            tasks[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task {j++}"));
+        
+        // так же без ожидания
+    }
+    
+    
 } 
