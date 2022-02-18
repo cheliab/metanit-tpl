c4e1435a519255e37a475ad1bd04d437fc30f6d0
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-18 11:25:36 +1000

    Пример цепочки заданий (ContinueWith)

---
+++ N_03_ContinuationTask/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace N_03_continationTask;
+
+// https://metanit.com/sharp/tutorial/12.3.php
+//
+// Задачи продолжения
+
+class Program
+{
+    public static void Main()
+    {
+        Task_ContinueWith();
+    }
+
+    /// <summary>
+    /// Использование метода ContinueWith
+    /// </summary>
+    private static void Task_ContinueWith()
+    {
+        // Первая задача в цепочке
+        Task task1 = new Task(() =>
+        {
+            Console.WriteLine($"Id задачи: {Task.CurrentId}");
+        });
+
+        // Вторая задача в цепочке - выполняется после task1
+        Task task2 = task1.ContinueWith(PrintTask);
+        
+        // Запускаем первую задачу
+        task1.Start();
+
+        // Ждем завершения второй задачи
+        task2.Wait();
+
+        Console.WriteLine("End of Main");
+    }
+
+    /// <summary>
+    /// Метод продолжения
+    /// </summary>
+    /// <param name="previousTask">Предыдущая задача</param>
+    private static void PrintTask(Task previousTask)
+    {
+        Console.WriteLine($"Id задачи: {Task.CurrentId}");
+        Console.WriteLine($"Id предыдущей задачи: {previousTask.Id}");
+    }
+}
